165453e76180b506f7fc81133cf532dd4b24a9ca
birkin <birkin.diana@gmail.com>
2017-03-27 13:29:44 -0400

    partially implements catch of bad utf-8 records.

--- exp_01.py
+++ exp_01.py
@@ -16,7 +16,7 @@ big_marc_filepath = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
 
 def count_records():
     """ Counts records in marc file.
-        Uses iterator so as not to have to store a huge amount of data in memory.
+        Uses iterator so as not to have to store a huge amount of data in memory (as enclosing the reader in a list would).
         The ```utf8_handling='ignore'``` is required to avoid a unicode-error, so trapping the errant-record isn't possible this way.
         """
     with open( big_marc_filepath, 'rb' ) as fh:
@@ -35,3 +35,31 @@ def count_records():
     log.debug( 'count of records in file, `{}`'.format(count) )
     log.debug( 'time_taken, `{}`'.format(end-start) )
 
+
+def count_records_and_log_bad_record():
+    """ Traps bad utf8 records that a for-loop can't handle.
+        Under construction.
+        """
+    with open( big_marc_filepath, 'rb' ) as fh:
+        start = datetime.datetime.now()
+        count = 0
+        while True:
+            try:
+                record = fh.readline()  # byte, not unicode-string
+            except Exception as e:
+                log.debug( 'exception accessing record-number ```{count}```; error, ```{err}```'.format(count=count, err=e) )
+                break
+            try:
+                record.decode( 'utf-8' )
+            except Exception as f:
+                log.debug( 'exception decoding record-number ```{count}```; error, ```{err}```'.format(count=count, err=f) )
+                log.debug( 'record, ```{}```'.format(record) )
+                break
+            count+=1
+            if count % 10000 == 0:
+                print( '`{}` records counted'.format(count) )
+            # if count > 2:
+            #     break
+    end = datetime.datetime.now()
+    log.debug( 'count of records in file, `{}`'.format(count) )
+    log.debug( 'time_taken, `{}`'.format(end-start) )
