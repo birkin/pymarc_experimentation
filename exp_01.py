a9713a9685d5b93323bd84dc1e112a2c1f7e1e6f
birkin <birkin.diana@gmail.com>
2017-03-28 09:45:24 -0400

    starts extract refactor.

--- exp_01.py
+++ exp_01.py
@@ -6,7 +6,6 @@ import pymarc
 
 log = logging.getLogger( 'pymarc_experimentation' )
 config_dct = json.loads( os.environ['PYMARC_EXP__LOG_CONFIG_JSON'] )
-config_dct['loggers']['pymarc_experimentation']['level'] = os.environ['PYMARC_EXP__LOG_LEVEL']
 logging.config.dictConfig( config_dct )
 log.debug( 'logging ready' )
 
@@ -17,6 +16,81 @@ logging.getLogger('TerminalIPythonApp').setLevel( logging.WARNING )
 
 
 big_marc_filepath = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
+
+
+class Extractor( object ):
+    """ Manages extraction of info from records in a marc file. """
+
+    def __init__( self ):
+        pass
+
+    def extract_info():
+        """ Prints/logs certain record elements.
+            The ```utf8_handling='ignore'``` is required to avoid a unicode-error.
+            """
+        log.debug( 'processing file, ``{}```'.format(big_marc_filepath) )
+        ( start, count ) = ( datetime.datetime.now(), 0 )
+        with open( big_marc_filepath, 'rb' ) as fh:
+            reader = pymarc.MARCReader( fh, force_utf8=True, utf8_handling='ignore' )  # w/o 'ignore', this line generates a unicode-error
+            for record in reader:
+                ( fields, title, bib_id, item_id, record_dct_logged ) = self.setup_main_loop( record )
+                for field_dct in fields:
+
+                    # ( bib_id, item_id ) = self.find_bib_and_item( bib_id, item_id )
+
+
+                    for (k, val_dct) in field_dct.items():
+                        if k == '907':
+                            try:
+                                bib_id = val_dct['subfields'][0]['a'][0:9]
+                            except Exception as e:
+                                log.debug( 'exception getting bib_id, ``{}```'.format(e) )
+                                log.debug( 'record_dct, ```{}```'.format( pprint.pformat(record_dct) ) )
+                                record_dct_logged = True
+                        if k == '945':
+                            try:
+                                subfields = val_dct['subfields']
+                                for subfield_dct in subfields:
+                                    for (k2, val2) in subfield_dct.items():
+                                        if k2 == 'y':
+                                            item_id = val2
+                            except Exception as f:
+                                log.debug( 'exception getting item_id, ``{}```'.format(f) )
+                                if record_dct_logged is False:
+                                    log.debug( 'record_dct, ```{}```'.format( pprint.pformat(record_dct) ) )
+                basic_info = {
+                    'title': record.title(), 'bib_id': bib_id, 'item_id': item_id }
+                # print( 'bas   ic_info, ```{}```'.format( pprint.pformat(basic_info) ) )
+                log.debug( 'basic_info, ```{}```'.format( pprint.pformat(basic_info) ) )
+                try:
+                    count+=1
+                    if count % 10000 == 0:
+                        print( '`{}` records processed'.format(count) )
+                    # if count > 100000:
+                    #     break
+                except Exception as e:
+                    log.debug( 'exception on record ```{rec}```; error, ```{err}```'.format(rec=record, err=e) )
+        end = datetime.datetime.now()
+        log.debug( 'count of records in file, `{}`'.format(count) )
+        log.debug( 'time_taken, `{}`'.format(end-start) )
+
+    def setup_main_loop( self, record ):
+        """ Initializes main processing loop.
+            Called by extract_info() """
+        record.force_utf8 = True
+        record_dct = record.as_dict()
+        fields = record_dct['fields']
+        title = record.title()
+        bib_id = 'not_available'
+        item_id = 'not_available'
+        record_dct_logged = False
+        return_tpl = ( fields, title, bib_id, item_id, record_dct_logged )
+        log.debug( 'return_tpl ( fields, title, bib_id, item_id, record_dct_logged ), ```{}```'.format(return_tpl) )
+        return return_tpl
+
+    ## end class Extractor()
+
+
 
 
 def extract_info():
