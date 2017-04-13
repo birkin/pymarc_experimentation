3f00120e60777826dbd88bf195e2d030c7a7b447
birkin <birkin.diana@gmail.com>
2017-04-13 14:14:52 -0400

    logs bad segments.

--- exp_py3.py
+++ exp_py3.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import datetime, json, logging, logging.config, os, pprint
+import datetime, json, logging, logging.config, os, pprint, sys
 import pymarc
 
 
+PROJECT_DIR = os.path.dirname( os.path.abspath(__file__) )
+sys.path.append( os.path.dirname(PROJECT_DIR) )
+from pymarc_experimentation import settings
+
+
 log = logging.getLogger( 'pymarc_experimentation' )
-config_dct = json.loads( os.environ['PYMARC_EXP__LOG_CONFIG_JSON'] )
+config_dct = settings.LOG_CONFIG_DCT
 logging.config.dictConfig( config_dct )
 log.debug( 'logging ready' )
 
@@ -19,7 +24,7 @@ class Extractor( object ):
     """ Manages extraction of info from records in a marc file. """
 
     def __init__( self ):
-        self.marc_filepath = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
+        self.marc_filepath = settings.INPUT_FILEPATH
         log.debug( 'processing file, ``{}```'.format(self.marc_filepath) )
         self.count = 0
         self.title = 'init'
@@ -120,8 +125,8 @@ def break_up_record( start_record=0, end_record=0 ):
     """ Splits big marc file into smaller files.
         This can successfully re-write the whole errant `rec_19.mrc` file. """
     log.debug( 'start_record, `{st}`; end_record, `{en}`'.format( st=start_record, en=end_record ) )
-    BIG_MARC_FILEPATH = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
-    SMALLER_OUTPUT_FILEPATH = os.environ['PYMARC_EXP__SMALLER_OUTPUT_MARC_FILEPATH']
+    BIG_MARC_FILEPATH = settings.INPUT_FILEPATH
+    SMALLER_OUTPUT_FILEPATH = settings.OUTPUT_FILEPATH
     log.debug( 'processing file, ``{}```'.format(BIG_MARC_FILEPATH) )
     log.debug( 'output file, ``{}```'.format(SMALLER_OUTPUT_FILEPATH) )
 
@@ -157,7 +162,7 @@ def extract_info():
     """ Prints/logs certain record elements.
         The ```utf8_handling='ignore'``` is required to avoid a unicode-error.
         """
-    big_marc_filepath = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
+    big_marc_filepath = settings.INPUT_FILEPATH
     log.debug( 'processing file, ``{}```'.format(big_marc_filepath) )
     with open( big_marc_filepath, 'rb' ) as fh:
         reader = pymarc.MARCReader( fh, force_utf8=True, utf8_handling='ignore' )  # w/o 'ignore', this line generates a unicode-error
@@ -217,7 +222,7 @@ def count_records():
         Uses iterator so as not to have to store a huge amount of data in memory (as enclosing the reader in a list would).
         The ```utf8_handling='ignore'``` is required to avoid a unicode-error, so trapping the errant-record isn't possible this way.
         """
-    big_marc_filepath = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
+    big_marc_filepath = settings.INPUT_FILEPATH
     with open( big_marc_filepath, 'rb' ) as fh:
         reader = pymarc.MARCReader( fh, force_utf8=True, utf8_handling='ignore' )  # w/o 'ignore', this line can generate a unicode-error
         start = datetime.datetime.now()
@@ -240,31 +245,54 @@ def count_records_and_log_bad_record():
         <https://github.com/edsu/pymarc/blob/master/test/reader.py>
         Under construction.
         """
-    big_marc_filepath = os.environ['PYMARC_EXP__BIG_MARC_FILEPATH']
+    big_marc_filepath = settings.INPUT_FILEPATH
     with open( big_marc_filepath, 'rb' ) as fh:
         start = datetime.datetime.now()
-        count = 0
+        fh.seek( 0, 2 ); file_size = fh.tell(); fh.seek( 0 )
+        log.debug( 'file_size(K), `{}`'.format( file_size/1024 ) )
+        count_processed = 0; count_good = 0; count_bad = 0
+        last_good_tell = 0
+        last_read_good = True
+        segment_to_review = 'init'
         reader = pymarc.MARCReader( fh, force_utf8=True, utf8_handling='ignore' )  # w/o 'ignore', this line can generate a unicode-error
-        while True:
+        process_flag = True
+        while process_flag is True:
+            # log.debug( 'fh.tell(), `{}`'.format( fh.tell() ) )
             try:
                 record = next( reader )
+                count_good += 1
+                if last_read_good is False:
+                    current_position = fh.tell()
+                    segment_to_review_byte_count = current_position - last_good_tell
+                    fh.seek( last_good_tell )
+                    segment_to_review = fh.read( segment_to_review_byte_count )
+                    log.debug( 'segment_to_review, ```{}```'.format(segment_to_review) )
+                    fh.seek( current_position )
+                last_good_tell = fh.tell()
+                last_read_good = True
                 # log.debug( 'type(record), `{}`'.format( type(record) ) )
             except Exception as e:
-                log.debug( 'exception accessing record-number ```{count}```; error, ```{err}```'.format(count=count, err=e) )
-                try:
-                    log.debug( 'record, ```{}```'.format(record) )
-                    record_dct = record.as_dict()
-                    log.debug( 'record_dct, ```{}```'.format( pprint.pformat(record_dct) ) )
-                except Exception as f:
-                    log.debug( 'exception processing record-number ```{count}```; error, ```{err}```'.format(count=count, err=f) )
-                    continue
-            count+=1
-            if count % 10000 == 0:
-                print( '`{}` records counted'.format(count) )
-            # if count > 2:
+                log.error( 'exception accessing record-number ```{count}```; error, ```{err}```'.format(count=count_processed, err=e) )
+                count_bad += 1
+                last_read_good = False
+                # try:
+                #     log.debug( 'record, ```{}```'.format(record) )
+                #     record_dct = record.as_dict()
+                #     log.debug( 'record_dct, ```{}```'.format( pprint.pformat(record_dct) ) )
+                # except Exception as f:
+                #     log.debug( 'exception processing record-number ```{count}```; error, ```{err}```'.format(count=count, err=f) )
+                #     continue
+            if fh.tell() == file_size:
+                process_flag = False
+            count_processed += 1
+            if count_processed % 10000 == 0:
+                print( '`{}` records counted'.format(count_processed) )
+            # if count_processed > 10000:
             #     break
     end = datetime.datetime.now()
-    log.debug( 'count of records in file, `{}`'.format(count) )
+    log.debug( 'count_processed, `{}`'.format(count_processed) )
+    log.debug( 'count_good, `{}`'.format(count_good) )
+    log.debug( 'count_bad, `{}`'.format(count_bad) )
     log.debug( 'time_taken, `{}`'.format(end-start) )
 
 
